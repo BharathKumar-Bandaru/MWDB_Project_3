3ea2fbe1e4606c43f81351b92d980e87e88c678d
Divya Dharani <divyadoll77@gmail.com>
2021-11-24 20:52:17 -0700

    Task 1,2,3 common code

--- tasks/task1_2_3.py
+++ tasks/task1_2_3.py
@@ -1,19 +1,57 @@
-from .input_output import get_images_with_attributes, get_image_arr_from_dict, store_array_as_csv
+from .input_output import *
 from .features import *
+from .dim_red import perform_dim_red
+from numpy import genfromtxt
+
+from .weight_matrices_calc import get_subject_weight_matrix, get_type_weight_matrix
 from .pca import compute_pca
 from .svd import compute_svd
 from .kmeans import kmeans
 from .lda import LDA
-from .dim_red import perform_dim_red
-
-from .weight_matrices_calc import get_subject_weight_matrix, get_type_weight_matrix
 
 
-# Entry for the task 1 and 2
-def task1_2(feature_model, filter, image_type, k, dim_red_technique,
+cache_for_input_images = {} #input_folder_path, {'images_with_attributes': , 'images':, 'image_features':, 'latent_semantics_file_path': }
+latent_semantics_file_id = 0
+
+# Entry for tasks 1,2, and 3
+def task_1_2_3(task_number, input_folder_path, feature_model, k, test_folder_path, classifier, label_type,
+               dim_red_technique = 'svd', output_folder = 'output', latent_semantics_file_name = None,
+               use_cached_input_images = True):
+
+    if use_cached_input_images is True and input_folder_path in cache_for_input_images:
+        images_with_attributes = cache_for_input_images[input_folder_path]['images_with_attributes']
+        image_features = cache_for_input_images[input_folder_path]['image_features']
+        latent_semantics_file_path = cache_for_input_images[input_folder_path]['latent_semantics_file_path']
+        latent_semantics = np.matrix(genfromtxt(latent_semantics_file_path, delimiter=','))
+    else:
+        images_with_attributes = get_images_and_attributes_from_folder(input_folder_path)
+        images = get_image_arr_from_dict(images_with_attributes)
+        image_features = get_flattened_features_for_images(images, feature_model)
+        dim_red_technique = dim_red_technique.lower()
+        left_factor_matrix, right_factor_matrix = perform_dim_red(dim_red_technique, image_features, k)
+        latent_semantics = right_factor_matrix
+
+        if latent_semantics_file_name is None:
+            latent_semantics_file_id += 1
+            latent_semantics_file_name = f'[{latent_semantics_file_id}]_task_{task_number}_{feature_model}_{dim_red_technique}_{k}_latent_semantics.csv'
+        store_array_as_csv(latent_semantics, output_folder, latent_semantics_file_name)
+
+        #Storing into cache
+        cache_for_input_images[input_folder_path] = {'images_with_attributes': images_with_attributes,
+                                                     'images': images,
+                                                     'image_features': image_features,
+                                                     'latent_semantics_file_path': os.path.join(output_folder, latent_semantics_file_name)}
+
+    test_images_with_attributes = get_images_and_attributes_from_folder(test_folder_path)
+    test_images = get_image_arr_from_dict(test_images_with_attributes)
+    test_image_features = get_flattened_features_for_images(test_images, feature_model)
+
+
+"""
+def task1_2_3(feature_model, filter, image_type, k, dim_red_technique,
             folder_path='input_images', output_folder='output',
             latent_semantics_file_name='task1_latent_semantics.csv'):
-    images_with_attributes = get_images_with_attributes(folder_path, filter=filter, filter_value=image_type)
+
     images = get_image_arr_from_dict(images_with_attributes)
     image_features = get_flattened_features_for_images(images, feature_model)
     if feature_model == "cm" and dim_red_technique == "lda":
@@ -58,3 +96,5 @@ def sort_print_matrix(subject_weight_matrix, k, one="s"):
         subject_weight_sort = sorted(subject_weight_sort.items(), key=lambda kv: kv[1], reverse=True)
         sorted_weights_for_each_latent_semantic[latent_sematic] = subject_weight_sort
         print(f"{latent_sematic} - {sorted_weights_for_each_latent_semantic[latent_sematic]}")
+
+"""
