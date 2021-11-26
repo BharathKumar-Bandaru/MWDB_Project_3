ff40c9dfc258e93555d3aeebe3aa1940db85e95f
Divya Dharani <divyadoll77@gmail.com>
2021-11-26 15:45:04 -0700

    cache updated

--- tasks/cache.py
+++ tasks/cache.py
@@ -1,4 +1,10 @@
-cache_for_input_images = {}
+cache_for_input_images = {} #{input_folder_path}_{feature_model}_{dim_red_technique}_{k}, {'images_with_attributes': , 'images':, 'image_features':, 'latent_semantics_file_path': }
+cache_for_folder_image_features = {} #input_folder_path: {'images': , 'image_features':}
 
 def get_cache_for_input_images():
     return cache_for_input_images
+
+def get_cache_for_folder_image_features():
+    return cache_for_folder_image_features
+
+
--- tasks/lsh.py
+++ tasks/lsh.py
@@ -1,3 +1,3 @@
-from cache import get_cache_for_input_images
+from cache import get_cache_for_folder_image_features
 
-cache_for_input_images = get_cache_for_input_images() #{input_folder_path}_{feature_model}_{dim_red_technique}_{k}, {'images_with_attributes': , 'images':, 'image_features':, 'latent_semantics_file_path': }
+get_cache_for_folder_image_features = get_cache_for_folder_image_features() #input_folder_path: {'images': , 'image_features':}
