8f3131b4d5953934743c2b7b9b58fc888e20bcb4
Aswinraj Ravichandran Prema <aswinraj.rp@gmail.com>
2021-11-26 16:59:14 -0700

    Update lsh.py

--- tasks/lsh.py
+++ tasks/lsh.py
@@ -8,8 +8,22 @@ class LocalitySensitiveHashing:
         number_of_features = len(input_vectors[0])
         self.random_planes = [np.random.randn(self.num_hashes_per_layer, number_of_features)
                               for i in range(self.num_layers)]
+        self.layer_buckets = [{} for i in range(self.num_layers)]
+        create_index_structure_with_input_vectors()
 
-    def get_hash_code_for_object(self, input_vector):
+    def create_index_structure_with_input_vectors(self):
+        input_vectors = self.input_vectors
+        layer_buckets = self.layer_buckets
+        for input_vector in input_vectors:
+            hash_codes = get_hash_codes_for_object(input_vector)
+            for i in range(self.num_layers):
+                buckets_dict = layer_buckets[i]
+                hash_code = hash_codes[i]
+                if hash_code not in buckets_dict:
+                    buckets_dict[hash_code] = []
+                buckets_dict[hash_code].append(input_vector)
+
+    def get_hash_codes_for_object(self, input_vector):
         hash_codes = []
         for layer_no in range(self.num_layers):
             hash_code = ""
@@ -21,3 +35,6 @@ class LocalitySensitiveHashing:
                     hash_code += '1'
             hash_codes.append(hash_code)
         return hash_codes
+
+    def get_layer_buckets(self):
+        return self.layer_buckets
