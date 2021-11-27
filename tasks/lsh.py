e7f34ba8b496f2404e6598a9ccff1df319e06349
Divya Dharani <divyadoll77@gmail.com>
2021-11-26 21:05:53 -0700

    lsh updated

--- tasks/lsh.py
+++ tasks/lsh.py
@@ -1,21 +1,23 @@
 import numpy as np
 from scipy.spatial import distance
+import sys
 
 class LocalitySensitiveHashing:
 
-    def __init__(self, num_layers, num_hashes_per_layer, image_objects):
+    def __init__(self, num_layers, num_hashes_per_layer, input_image_objects):
         self.num_layers = num_layers
         self.num_hashes_per_layer = num_hashes_per_layer
-        self.image_objects = image_objects
-        number_of_features = len(image_objects[0].features)
+        self.input_image_objects = input_image_objects
+        number_of_features = len(input_image_objects[0].features)
         self.random_planes = [np.random.randn(self.num_hashes_per_layer, number_of_features)
                               for i in range(self.num_layers)]
         self.hash_buckets_per_layer = [{} for i in range(self.num_layers)]
         self.create_index_structure_with_input_vectors()
+        self.num_buckets_searched = 0
 
     def create_index_structure_with_input_vectors(self):
         hash_buckets_per_layer = self.hash_buckets_per_layer
-        for idx, image_obj in enumerate(self.image_objects):
+        for idx, image_obj in enumerate(self.input_image_objects):
             input_vector = image_obj.features
             hash_codes = self.get_hash_codes_for_object(input_vector)
             for i in range(self.num_layers):
@@ -40,7 +42,7 @@ class LocalitySensitiveHashing:
 
     def retrieve_objects_in_bucket(self, layer_num, hashcode):
         object_indices = self.hash_buckets_per_layer[layer_num][hashcode]
-        return [self.image_objects[index] for index in object_indices]
+        return [self.input_image_objects[index] for index in object_indices]
 
     def get_hash_buckets_per_layer(self):
         return self.hash_buckets_per_layer
@@ -56,11 +58,20 @@ class LocalitySensitiveHashing:
         return result
 
     def get_similar_objects(self, query_image_obj, num_similar_images_to_retrieve):
+        self.num_buckets_searched = 0
+        self.overall_images_considered = 0
+        self.unique_images_considered = 0
+
         hash_codes = self.get_hash_codes_for_object(query_image_obj.features)
+        print(f'Hash codes of the query image in different layers: {hash_codes}')
         image_set = set()
         for idx, hash_code in enumerate(hash_codes):
             images = self.retrieve_objects_in_bucket(idx, hash_code)
             image_set.update(images)
+            self.num_buckets_searched += 1
+            self.overall_images_considered += len(images)
+
+        self.unique_images_considered = len(image_set)
         image_object_list = list(image_set)
         image_indices_and_dist = self.get_image_indices_with_distances(query_image_obj, image_object_list)
         image_indices_and_dist.sort(key = lambda x: x[1])
@@ -73,4 +84,20 @@ class LocalitySensitiveHashing:
 
         return result_image_objects
 
+    def print_index_structure_stats(self):
+        print('LSH Index Structure Stats:')
+        hash_buckets_per_layer = self.hash_buckets_per_layer
 
+        print('No. of objects per hash code in each layer:')
+        for i in range(len(hash_buckets_per_layer)):
+            print('-----------')
+            print(f'Layer {i + 1}:')
+            hash_buckets_dict = hash_buckets_per_layer[i]
+            for key in hash_buckets_dict:
+                print(f'{key}: {len(hash_buckets_dict[key])}')
+
+        print(f'Total index structure size in bytes: {sys.getsizeof(hash_buckets_per_layer)}')
+        print(f'Number of buckets searched: {self.num_buckets_searched}')
+        print(f'Number of input images: {len(self.input_image_objects)}')
+        print(f'Number of overall images considered: {self.overall_images_considered}')
+        print(f'Number of unique images considered: {self.unique_images_considered}')
