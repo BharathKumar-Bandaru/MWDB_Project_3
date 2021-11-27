5fa36c28d871a5b69070efecc93c02d33ed33f31
Divya Dharani <divyadoll77@gmail.com>
2021-11-26 17:06:10 -0700

    Updated task4

--- tasks/task4.py
+++ tasks/task4.py
@@ -5,4 +5,8 @@ from lsh import LocalitySensitiveHashing
 def task4(input_folder, feature_model, num_layers, num_hashes_per_layer, query_image_path, num_similar_images_to_retrieve):
     images_with_attributes, image_features  = cache.get_images_with_attributes_and_features(input_folder, feature_model)
     lsh_obj = LocalitySensitiveHashing(num_layers, num_hashes_per_layer, image_features)
-    
+
+
+    print(lsh_obj.get_layer_buckets())
+
+
