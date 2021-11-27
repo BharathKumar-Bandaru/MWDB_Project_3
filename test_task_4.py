af528395cdf9c7cb185bc67bbaaec595a08a45cc
Divya Dharani <divyadoll77@gmail.com>
2021-11-26 22:42:54 -0700

    test task4 file updated

--- test_task_4.py
+++ test_task_4.py
@@ -4,4 +4,4 @@ from tasks.lsh import *
 from tasks.task4 import task4
 
 task4(input_folder='Dataset', feature_model='elbp', num_layers=5, num_hashes_per_layer=3,
-      query_image_path=os.path.join('Dataset','image-con-35-3.png'), num_similar_images_to_retrieve=20)
+      query_image_path=os.path.join('Dataset','image-con-35-3.png'), num_similar_images_to_retrieve=10)
