84eae9d9a52d90e615d30b73168473ea188ed82e
Divya Dharani <divyadoll77@gmail.com>
2021-11-26 17:25:49 -0700

    task4 updated

--- tasks/task4.py
+++ tasks/task4.py
@@ -6,7 +6,11 @@ def task4(input_folder, feature_model, num_layers, num_hashes_per_layer, query_i
     images_with_attributes, image_features  = cache.get_images_with_attributes_and_features(input_folder, feature_model)
     lsh_obj = LocalitySensitiveHashing(num_layers, num_hashes_per_layer, image_features)
 
+    hash_buckets_per_layer = lsh_obj.get_hash_buckets_per_layer()
+    for hash_buckets_dict in hash_buckets_per_layer:
+        for key in hash_buckets_dict:
+            print(key, len(hash_buckets_dict[key]))
 
-    print(lsh_obj.get_layer_buckets())
+
 
 
