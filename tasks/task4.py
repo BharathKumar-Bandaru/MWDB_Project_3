c878c956d2823128c12d735dac846d890fb7a52d
Divya Dharani <divyadoll77@gmail.com>
2021-11-26 17:41:47 -0700

    Task 4 - updated

--- tasks/task4.py
+++ tasks/task4.py
@@ -1,13 +1,19 @@
 from .cache import *
 from .lsh import LocalitySensitiveHashing
-
+from .input_output import *
 
 def task4(input_folder, feature_model, num_layers, num_hashes_per_layer, query_image_path, num_similar_images_to_retrieve):
     images_with_attributes, image_features  = get_images_with_attributes_and_features(input_folder, feature_model)
-    lsh_obj = LocalitySensitiveHashing(num_layers, num_hashes_per_layer, image_features)
+    image_objects = get_image_objects_from_dict(images_with_attributes, image_features)
+    lsh_obj = LocalitySensitiveHashing(num_layers, num_hashes_per_layer, image_objects)
 
+    print(images_with_attributes)
+    ##Testing##
     hash_buckets_per_layer = lsh_obj.get_hash_buckets_per_layer()
-    for hash_buckets_dict in hash_buckets_per_layer:
+    for i in range(len(hash_buckets_per_layer)):
+        print('-----------')
+        print(f'Layer {i+1}')
+        hash_buckets_dict = hash_buckets_per_layer[i]
         for key in hash_buckets_dict:
             print(key, len(hash_buckets_dict[key]))
 
