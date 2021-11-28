9ff05a28bdab8cae58ca2c293c62b9f85411bbe1
BharathKumar-Bandaru <bbandaru@asu.edu>
2021-11-28 00:54:55 -0700

    Updated code for integration

--- tasks/task8.py
+++ tasks/task8.py
@@ -4,11 +4,11 @@ from tasks.custom_svm import *
 from tasks.svm import *
 from tasks.task4 import *
 from tasks.decision_trees import *
-from scipy.spatial.distance import cityblock
+from scipy.spatial.distance import cityblock, euclidean
 
 
 def task8(task_number):
-    output_folder_7 = "svm_feedback_output_for_task_"
+    output_folder_7 = "_feedback_output_for_task_"
 
     output_image_features = []
     perform_task_6_7_flag = False
@@ -46,10 +46,10 @@ def task8(task_number):
         print("Enter the number of bits(b):")
         b = int(input())
 
-        test_image = get_images_and_attributes_from_folder(test_image_path)
-        q_image_name = os.path.join(test_image_path, test_image[0]['filename'])
+        q_image_name = os.path.join(test_image_path, test_image_name)
 
-        output_image_features = perform_va_files(folder_path, feature, test_image_path, t, b)
+
+        output_image_features = perform_va_files(folder_path, feature, q_image_name, t, b)
         print("\nDo you want to perfrom task 6 and 7? [y or n]:")
         key = input()
         perform_task_6_7_flag = True if key == "y" else False
@@ -58,38 +58,63 @@ def task8(task_number):
         print("Enter task 6 or 7:")
         task = int(input())
         query_image = get_image_arr_from_file(q_image_name)
-        query_image_features = get_flattened_features_for_images(query_image, feature)
+        query_image_features = get_flattened_features_for_images([query_image], feature)
+        query_image_features = query_image_features[0]
 
         image_features = np.array([each[2] for each in output_image_features])
         image_names = np.array([each[1] for each in output_image_features])
         images = np.array([each[0] for each in output_image_features])
+        # icecream.ic(image_features.shape, image_names.shape, images.shape, image_names)
 
     if perform_task_6_7_flag and task == 6:
-        predicted_labels = []
-        X_data, new_labels = mark_relevant_non_relevant(output_image_features)
-        X_data = add_label_to_image_arr(X_data, new_labels)
+        relevant_images, non_relevant_images, relevant_images_id, non_relevant_image_id = mark_relevant_non_relevant(
+            output_image_features)
+
+        X_data = np.append(relevant_images, non_relevant_images, axis=0)
+        label = np.array([1] * len(relevant_images_id) + [0] * len(non_relevant_image_id))
+
+        idx = np.random.permutation(len(X_data))
+        X_data, new_labels = X_data[idx], label[idx]
+
+        l, right_factor_matrix = perform_dim_red("pca", X_data, 5)
+        X_data = np.array(np.matmul(X_data, np.transpose(right_factor_matrix)))
+        image_features = np.array(np.matmul(image_features, np.transpose(right_factor_matrix)))
+
+        dataset_features = add_label_to_image_arr(X_data, new_labels)
 
         dc = DecisionTreeClassifier()
-        root = dc.make_tree(X_data)
+        root = dc.make_tree(dataset_features)
 
-        images_with_attributes = get_images_and_attributes_from_folder(folder_path)
-        images = [image_dict['image'] for image_dict in images_with_attributes]
-        image_features = get_flattened_features_for_images(images, feature)
-        image_names = [image_dict['filename'] for image_dict in images_with_attributes]
-
-        count = 0
-        totalcount = 0
+        predicitons = []
         for i in range(len(image_features)):
             pre = dc.predict(image_features[i], root)
-            predicted_labels.append(pre)
+            predicitons.append(pre)
+        predicitons = np.array(predicitons)
 
-        revelant_images = []
-        for i in range(len(predicted_labels)):
-            if predicted_labels[i] == 1:
-                revelant_images.append(image_features[i])
+        relevant_test_images = np.where(predicitons == 1)
+        non_relevant_test_images = np.where(predicitons == 0)
+        query_image_features = np.array(np.matmul(query_image_features, np.transpose(right_factor_matrix)))
 
+        ordered_relevant = get_top_k(query_image_features, image_features[relevant_test_images],
+                                     image_names[relevant_test_images], images[relevant_test_images])
+        ordered_irrelevant = get_top_k(query_image_features, image_features[non_relevant_test_images],
+                                       image_names[non_relevant_test_images], images[non_relevant_test_images])
 
-        results = get_top_k(test_image[0]['image'])
+        final_predictions = ordered_relevant[:t]
+
+        if len(final_predictions) < t - 1:
+            final_predictions = np.append(final_predictions, ordered_irrelevant[: t - len(final_predictions)], axis=0)
+
+        for i in final_predictions:
+            print(i[1])
+        output_folder_7 = f"decision_tree{output_folder_7}{task_number}"
+        img_result = []
+        for i, each in enumerate(final_predictions):
+            img_result.append((each[2], f"{i + 1}_{each[1]}"))
+        print("The similar images after decision tree classifier.")
+
+        save_images_by_clearing_folder(img_result, output_folder_7)
+
 
     elif perform_task_6_7_flag and task == 7:
         relevant_images, non_relevant_images, relevant_images_id, non_relevant_image_id = mark_relevant_non_relevant(output_image_features)
@@ -104,13 +129,56 @@ def task8(task_number):
         svm.train(X_data, new_labels)
 
         predicitons = svm.predict(image_features)
+
         relevant_test_images = np.where(predicitons == 1)
         non_relevant_test_images = np.where(predicitons == 0)
 
         ordered_relevant = get_top_k(query_image_features, image_features[relevant_test_images], image_names[relevant_test_images], images[relevant_test_images])
         ordered_irrelevant = get_top_k(query_image_features, image_features[non_relevant_test_images], image_names[non_relevant_test_images], images[non_relevant_test_images])
 
-        final_predictions = ordered_irrelevant[:t]
+        final_predictions = ordered_relevant[:t]
+
+        if len(final_predictions) < t-1:
+            final_predictions = np.append(final_predictions, ordered_irrelevant[: t - len(final_predictions)], axis=0)
+
+        for i in final_predictions:
+            print(i[1])
+
+        output_folder_7 = f"svm{output_folder_7}{task_number}"
+        img_result = []
+        for i, each in enumerate(final_predictions):
+            img_result.append((each[2], f"{i+1}_{each[1]}"))
+        save_images_by_clearing_folder(img_result, output_folder_7)
+
+        # weights = svm.get_weights(image_features)
+        # max_ = np.argmax(weights, axis=1)
+        # weights_ = []
+        # weights_non_ = []
+        # for i, val in enumerate(max_):
+        #     if val == 1:
+        #         weights_.append([weights[i][1], i])
+        #     else:
+        #         weights_non_.append([weights[i][0], i])
+        # weights_ = sorted(weights_, key = lambda t: t[0], reverse=True)
+        # weights_non_ = sorted(weights_non_, key = lambda t: t[0], reverse=True)
+        #
+        # img_result = []
+        # print("-----------")
+        # print(f"The top {t} images for input image {q_image_name}.")
+        # count = 1
+        # for i in weights_[:t]:
+        #     img_result.append((images[i[1]], f'{count}_{image_names[i[1]]}'))
+        #     print(image_names[i[1]])
+        #     count+=1
+        # if(len(img_result) < t):
+        #     count = 0
+        #     while(len(img_result) <= t):
+        #         img_result.append((images[weights_non_[count][1]], f'{count}_{image_names[weights_non_[count][1]]}'))
+        #         print(image_names[weights_non_[count][1]])
+        #         count+=1
+
+
+        # icecream.ic(final_predictions)
 
         # icecream.ic([out[1] for out in output_image_features])
         # image_features = np.array([out[2] for out in output_image_features[10:]])
@@ -159,7 +227,8 @@ def task8(task_number):
 
 def mark_relevant_non_relevant(output_images):
     image_names = [f"{i+1}. {each[1]}" for i, each in enumerate(output_images)]
-    print(image_names)
+    for i in image_names:
+        print(i)
 
     print("Enter the relevant image id's from the above list:")
     relevant_images_id = np.array(input().split()).astype(int) - 1
@@ -169,6 +238,11 @@ def mark_relevant_non_relevant(output_images):
 
     revelant_images = np.array([each[2] for each in np.array(output_images)[relevant_images_id]])
     non_revelant_images = np.array([each[2] for each in np.array(output_images)[non_relevant_image_id]])
+
+    print("Relevant Images:")
+    print(np.array([each[1] for each in np.array(output_images)[relevant_images_id]]))
+    print("Irr-Relevant Images:")
+    print(np.array([each[1] for each in np.array(output_images)[non_relevant_image_id]]))
 
     return revelant_images, non_revelant_images, relevant_images_id, non_relevant_image_id
 
@@ -190,7 +264,7 @@ def mark_relevant_non_relevant(output_images):
 def get_top_k(q_image, image_features, image_names, image):
     top_k_images = []
     for i in range(len(image_features)):
-        top_k_images.append([cityblock(q_image, image_features[i]), image_names[i], image[i]])
+        top_k_images.append([euclidean(q_image, image_features[i]), image_names[i], image[i]])
 
     top_k_images = sorted(top_k_images, key = lambda k: k[0])
-    return top_k_images
+    return np.array(top_k_images)
--- tasks/vafiles.py
+++ tasks/vafiles.py
@@ -1,9 +1,6 @@
 import math
 from tasks.input_output import *
-import numpy as np
-from tasks.features import *
-
-
+total_buckets = []
 def compute_number_of_bits(b, d):
     # v = number of vectors
     # b = number of bits
@@ -58,22 +55,30 @@ def bin(n, length):
 
 def compute_region(vector, partition_points):
     regions = []
-    # print(len(vector))
+
+    # buckets_list = []
     for j in range(0, len(vector)):
         flag = 0
         region = 0
-        # print("Length of partition points is: ", len(partition_points[j]))
+
         for i in range(0, len(partition_points[j])-1):
+            total_buckets.append(partition_points[j][i])
+            #buckets_list.append(partition_points[j][i])
             if(partition_points[j][i] <= int(vector[j]) and int(vector[j]) < partition_points[j][i+1]):
                 regions.append(i)
                 flag = 1
-                break
+                break;
         #Handle edge case
-        if(flag == 0):
-            if(int(vector[j]) < partition_points[j][0]):
+
+        if flag == 0:
+            if vector[j] < partition_points[j][0]:
+                total_buckets.append(partition_points[j][i])
+                # buckets_list.append(partition_points[j][0])
                 regions.append(partition_points[j][0])
-            elif(int(vector[j]) >= partition_points[j][len(partition_points)-1]):
-                regions.append(partition_points[j][len(partition_points)-1])
+            elif vector[j] >= partition_points[j][len(partition_points[j])-1]:
+                total_buckets.append(partition_points[j][i])
+                # buckets_list.append(partition_points[j][len(partition_points[j])-1])
+                regions.append(partition_points[j][len(partition_points[j])-1])
 
 
     return regions
@@ -85,19 +90,21 @@ def compute_bit_string(vector, partition_points, bits):
         flag = 0
 
         for i in range(0, len(partition_points[j])-1):
-            if (partition_points[j][i] <= vector[j] and vector[j] < partition_points[j][i + 1]):
+
+            if partition_points[j][i] <= vector[j] and vector[j] < partition_points[j][i + 1]:
                 regions.append(i)
                 bit_string = bin(partition_points[j][i], bits[j])
                 final_rep = final_rep + bit_string
                 flag = 1
                 break
         # Handle edge case
-        if (flag == 0):
-            if (vector[j] < partition_points[j][0]):
+        if flag == 0:
+
+            if vector[j] < partition_points[j][0]:
                 regions.append(partition_points[j][0])
                 bit_string = bin(partition_points[j][0], bits[j])
                 final_rep = final_rep + bit_string
-            elif (vector[j] >= partition_points[j][len(partition_points[j]) - 1]):
+            elif vector[j] >= partition_points[j][len(partition_points[j]) - 1]:
                 regions.append(partition_points[j][len(partition_points[j]) - 1])
                 bit_string = bin(partition_points[j][len(partition_points[j]) - 1], bits[j])
                 final_rep = final_rep + bit_string
@@ -132,6 +139,7 @@ def compute_manhattan(v1, v2):
     for i in range(0, len(v1)):
         dist += abs(v1[i]-v2[i])
     return dist
+
 def Candidate(d, i, dst, ans, t):
     if d < dst[t-1]:
         dst[t-1] = d
@@ -145,30 +153,34 @@ def Candidate(d, i, dst, ans, t):
 def simple_search_algorithm(feature_vectors, vq, t, partition_points):
     ans = []
     dst = []
-    buckets = 0
+    images_considered = 0
     for k in range(0, t):
         dst.append(float('inf'))
         ans.append(0)
 
-
+    # total_buckets = []
     d = float('inf')
     idx = 0
     for i in range(0, len(feature_vectors)):
         li = compute_lower_bound(feature_vectors[i], vq, partition_points)
+        # total_buckets += buckets
+        # for b in buckets:
+        #     total_buckets.append(b)
+
 
         if li < d or idx < t:
-            buckets += 1
+            images_considered += 1
             d = compute_euclidean(vq, feature_vectors[i])
             idx+=1
             ans, dst = Candidate(d, i, dst, ans, t)
 
-    return ans, dst, buckets
+    return ans, dst, len(set(total_buckets)), images_considered
 
 def calculate_distances(original, test):
     result = {}
     i = 0
     for img in original:
-        result[i] = compute_manhattan(img, test)
+        result[i] = compute_euclidean(img, test)
         i+=1
 
     actual_result = sorted(result.items(), key=lambda x: x[1])
@@ -176,19 +188,28 @@ def calculate_distances(original, test):
     return actual_result
 
 
-def perform_va_files(folder_path, feature, test_image_path, t, b):
+def perform_va_files(folder_path, feature, q_image_name, t, b):
+    print("Calculating features vectors of the dataset")
     image_data_attributes = get_images_and_attributes_from_folder(folder_path)
     images = [img['image'] for img in image_data_attributes]
     image_names = [img['filename'] for img in image_data_attributes]
     image_features = get_flattened_features_for_images(images, feature)
 
+    # q_image = get_image_arr_from_file(q_image_name)
+    # query_image_features = get_flattened_features_for_images([q_image], feature)
+
     d = len(image_features[0])
+    print("Computing bits per dimension: ")
     bits = compute_number_of_bits(b, d)
     overall_partition_list = []
+    """Calculate the partition points for each dimension. These points are added to the overall_partition_points list
+    which will contains the partition points for all dimensions."""
+
     for i in range(d):
         partition_points = distribute_partition_points(image_features, i, bits)
         overall_partition_list.append(partition_points)
 
+    """Calculate approximation vectors for each vector in the Database"""
     approximations = []
     print("Calculating approximations: ")
     for i in range(0, len(image_features)):
@@ -196,15 +217,17 @@ def perform_va_files(folder_path, feature, test_image_path, t, b):
         approximations.append(bit_string)
     approx_set = set(approximations)
 
-    test_dataset = get_images_and_attributes_from_folder(test_image_path)
+    """Read the query image and compute its feature vecyor"""
+    test_dataset = get_images_and_attributes_from_folder(q_image_name)
     actual = []
     test_type = test_dataset[0]['type']
     test_subject_id = test_dataset[0]['subject_id']
-    test_image_id = test_dataset[0]['image_id']
     test_file_name = test_dataset[0]['filename']
     actual.append(test_type)
     actual.append(test_subject_id)
     query_vector = np.array(get_flattened_features_for_images([test_dataset[0]['image']], feature))
+
+    print("Performing Simple Search Algorithm: ")
     ans, dst, buckets = simple_search_algorithm(image_features, query_vector[0], t, overall_partition_list)
     expected_files = []
     actual_result = calculate_distances(image_features, query_vector[0])
@@ -221,17 +244,17 @@ def perform_va_files(folder_path, feature, test_image_path, t, b):
     output = []
     output_features = []
     output.append((test_dataset[0]['image'], "input-" + test_file_name))
+
     false_positives = 0
     misses = 0
-    count = 0
+    correct_images = 0
     for a in ans:
-        if (image_data_attributes[a]['type'] not in actual and image_data_attributes[a]['subject_id'] not in actual):
+        if image_data_attributes[a]['filename'] not in expected_files:
             false_positives += 1
-        if (image_data_attributes[a]['filename'] not in expected_files):
-            misses += 1
+        else:
+            correct_images += 1
         output.append((images[a], image_names[a]))
         output_features.append((images[a], image_names[a], image_features[a]))
-        count+=1
     save_images_by_clearing_folder(output, "Output_VAFiles")
 
     print("Number of unique approximations considered are: ", len(approx_set))
@@ -243,48 +266,45 @@ def perform_va_files(folder_path, feature, test_image_path, t, b):
     return output_features
 
 
-# #Test function
-# #read dataset from folder path. Output is a list of dictionaries
-# image_data = get_images_and_attributes_from_folder("Dataset")
+# image_data = get_images_and_attributes_from_folder("4000")
 # images = []
 # file_names = []
 # for img_dict in image_data:
 #     images.append(img_dict['image'])
 #     file_names.append(img_dict['filename'])
 # feature_vetors = []
+# print("Calculating features vectors of the dataset")
 # for im in images:
-#     input_vector = computer_hog(im)
+#     input_vector = compute_color_moment_of_image(im)
 #     feature_vetors.append(input_vector)
-#
-#
-# #take b value as input"
-# b = 5000
-#
+
+
+
+
+
 # d = len(feature_vetors[0])
-#
+
+# b = 800
+# print("Computing bits per dimension: ")
 # bits = compute_number_of_bits(b, d)
-#
+
 # overall_partition_list = []
-# #Get partitionpartition_points[len(partition_points)-1] points for each dimension
-# #overall_partition_list contains patition points list for every dimension.
-# print(len(bits))
-# print(len(feature_vetors[0]))
-#
+# """Calculate the partition points for each dimension. These points are added to the overall_partition_points list
+# which will contains the partition points for all dimensions."""
+
 # for i in range(0, d):
 #     partition_points = distribute_partition_points(feature_vetors, i, bits)
-#     #print(partition_points)
 #     overall_partition_list.append(partition_points)
 #
-#
-# #Get approximations for each vector/datapoint
+# """Calculate approximation vectors for each vector in the Database"""
 # approximations = []
 # print("Calculating approximations: ")
 # for i in range(0, len(feature_vetors)):
 #     bit_string, regions = compute_bit_string(feature_vetors[i], overall_partition_list, bits)
 #     approximations.append(bit_string)
 # approx_set = set(approximations)
-#
-#
+
+# """Read the query image and compute its feature vecyor"""
 # test_dataset = get_images_and_attributes_from_folder("TestVA")
 # actual = []
 # test_type = test_dataset[0]['type']
@@ -293,13 +313,17 @@ def perform_va_files(folder_path, feature, test_image_path, t, b):
 # test_file_name = test_dataset[0]['filename']
 # actual.append(test_type)
 # actual.append(test_subject_id)
-#
-# query_vector = computer_hog(test_dataset[0]['image'])
+
+# query_vector = compute_color_moment_of_image(test_dataset[0]['image'])
 # t = 10
-# ans, dst, buckets = simple_search_algorithm(feature_vetors, query_vector, t, overall_partition_list)
+# print("Performing Simple Search Algorithm: ")
+
+
+# ans, dst, total_buckets, images_considered = simple_search_algorithm(feature_vetors, query_vector, t, overall_partition_list)
+#
 # expected_files = []
 # actual_result = calculate_distances(feature_vetors, query_vector)
-#
+
 # idx = 0
 # print("Expected outputs are: ")
 # for x in actual_result:
@@ -308,24 +332,24 @@ def perform_va_files(folder_path, feature, test_image_path, t, b):
 #     idx+=1
 #     if idx == t:
 #         break
-#
-#
-# print("Output vectors are saved to the Output_VAFiles folder: ")
+# print("Output images are saved to the Output_VAFiles folder. ")
 # output = []
 # output.append((test_dataset[0]['image'], "input-"+test_file_name))
 # false_positives = 0
 # misses = 0
+# correct_images = 0
 # for a in ans:
-#     if(image_data[a]['type'] not in actual and image_data[a]['subject_id'] not in actual):
+#     if image_data[a]['filename'] not in expected_files:
 #         false_positives += 1
-#     if(image_data[a]['filename'] not in expected_files) :
-#         misses += 1
+#     else:
+#         correct_images += 1
 #     output.append((images[a], file_names[a]))
 # save_images_by_clearing_folder(output, "Output_VAFiles")
+#
 #
 # print("Number of unique approximations considered are: ", len(approx_set))
 # print("Number of overall approximations considered are ", len(approximations))
 # print("Number of bytes required for index structure is: ", len(approximations)*len(approximations[0])/8)
-# print("Number of buckets searched are: ", buckets)
-# print("The number of false positives are: ",false_positives)
-# print("The miss rate is: ",misses/len(expected_files))
+# print("Number of buckets searched are: ", total_buckets)
+# print("The number of false positives are: ",(images_considered-correct_images)/images_considered)
+# print("The miss rate is: ",(t-correct_images)/t)
